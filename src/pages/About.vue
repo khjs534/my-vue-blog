8118465ba7f2b13ce9060095da3e0e3522f7d0e7
Sean Kung <email": null>
2019-02-20 13:37:40 -0600

    Updates about page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout>
     <h1>About me</h1>
-    <p>I'm a Software Developer, and former electrical engineer interested in what the future of technology holds and how it will affect societal constructs.</p>
+    <p>I'm Sean Kung, a Software Developer and former electrical engineer, interested in what the future of technology holds and how it will affect societal constructs.</p>
     <p>As a former electrical engineering I have experience in electrical safety testing and hardware startups from design to production.</p>
     <p>I am passionate about beautiful and elegant code that does inspiring things.</p>
   </Layout>
